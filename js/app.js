4a54aca83dc5733b4675df76d48270678fbead17
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-02-07 14:29:49 +0545

    now can add items phase 5

--- js/app.js
+++ js/app.js
@@ -1,18 +1,34 @@
 // ....
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
-
+import { createForm } from "./form.js";
 let items = groceryItems;
 
 // Render App
 function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
-
+  const formElement = createForm();
   const itemsElement = createItems(items);
+  app.appendChild(formElement);
   app.appendChild(itemsElement);
 }
+ // Generate unique ID
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
 
+// Add Item Function
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
+}   
 // Initialize App
 render();
 // Edit Completed Function
